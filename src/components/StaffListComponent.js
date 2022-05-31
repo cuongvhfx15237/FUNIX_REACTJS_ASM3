92abcaeb9a35fa8c5e829f5675b1fc62e1221f9e
Vo Huy Cuong <cuongvhfx15237@funix.edu.vn>
2022-05-31 16:35:26 +0700

    ok get value of validation

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,5 +1,17 @@
-import React, { useState, useEffect } from "react";
-import { Card, CardImg, CardTitle, Form, FormGroup, Label, Input, ModalHeader, ModalBody, ModalFooter, FormFeedback } from "reactstrap";
+import React, { useState } from "react";
+import {
+  Card,
+  CardImg,
+  CardTitle,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  FormFeedback,
+} from "reactstrap";
 import "../index.css";
 import { Link } from "react-router-dom";
 import { Button, Modal } from "reactstrap";
@@ -9,79 +21,121 @@ function StaffList(props, iStaffs, submitForm) {
   const [modalAdd, setModalAdd] = useState(false);
   const toggle1 = () => setModalAdd(!modalAdd);
 
-  const [name, setName]= useState("");
-  const [doB, setDoB]= useState("");
-  const [startDate, setStartDate]= useState("");
-  const [department, setDepartment]= useState("Sale");
-  const [salaryScale, setSalaryScale]= useState("1");
-  const [annualLeave, setAnnualLeave]= useState("0");
-  const [overTime, setOverTime]= useState("0");
-  const [image, setImage] = useState("/assets/images/alberto.png");
-  const [touched, setTouched] = useState({
-              name: false,
-              doB: false,
-              startDate: false,
-              salaryScale: false,
-              department: false,
-              annualLeave: false,
-              overTime: false,
-  })
-  function handleBlur(field){
-  
-      setStartDate({
-        touched: {...touched, [field]: true}
-      })
-
-  }
-  function handleInputChange(event){
-    const target=event.target;
-    const value=target.value;
-    const name=target.name;
-    setStartDate({
-      [name]: value
-    })
-
-  }
-  function handleSubmit(){
-    const newStaff = {
-      name: name,
-      doB: doB,
-      startDate: startDate,
-      department: department,
-      salaryScale: salaryScale,
-      annualLeave: annualLeave,
-      overTime: overTime,
-      image: image
+  const [state, setState] = useState({
+    name:"",
+    doB: "",
+    startDate:"",
+    department:"Sale",
+    salaryScale:1,
+    annualLeave:0,
+    overTime:0,
+    imate:"/assets/images/alberto.png",
+    touched: {
+      name: false,
+      doB: false,
+      startDate: false,
+      salaryScale: false,
+      department: false,
+      annualLeave: false,
+      overTime: false,
     }
-    // onAdd
+  })
+  // const [name, setName] = useState("");
+  // const [doB, setDoB] = useState("");
+  // const [startDate, setStartDate] = useState("");
+  // const [department, setDepartment] = useState("Sale");
+  // const [salaryScale, setSalaryScale] = useState("1");
+  // const [annualLeave, setAnnualLeave] = useState("0");
+  // const [overTime, setOverTime] = useState("0");
+  // const [image, setImage] = useState("/assets/images/alberto.png");
+  // const [touched, setTouched] = useState({
+  //   name: false,
+  //   doB: false,
+  //   startDate: false,
+  //   salaryScale: false,
+  //   department: false,
+  //   annualLeave: false,
+  //   overTime: false,
+  // });
+  const handleBlur=(field)=> (event)=> {
+      event.preventDefault();
+    setState({ 
+      touched: {[event] : true },
+    });
   }
- function validate(name, doB, startDate, department, salaryScale, annualLeave, overTime){
-   const errors = {
-     name:"",
-     doB:"",
-     startDate:"",
-     department:"",
-     salaryScale:"",
-     annualLeave:"",
-     overTime:"",
-   };
-   if (touched.name && name.length <3){
-   errors.name = "name should be >=3 chars";}
-  else if ( touched.name && name.length>50){
-  errors.name = " name should be <= 50 chars";}
-  if (touched.department && department.length <1){
-  errors.department = "Vui lòng nhập thông tin"}
-  if (touched.salaryScale && salaryScale.length <1){
-  errors.salaryScale = "Vui lòng nhập thông tin"}
-  if (touched.annualLeave && annualLeave.length <1){
-  errors.annualLeave = "Vui lòng nhập thông tin"}
-  if (touched.overTime && overTime.length <1){
-  errors.overTime = "Vui lòng nhập thông tin"}
-  if (touched.startDate && startDate.length <1){
-  errors.startDate = "Vui lòng nhập thông tin"}
-  if (touched.doB && doB.length <1){
-  errors.doB = "Vui lòng nhập thông tin";}
-  return errors;
+  function handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    setState({
+      [name]: value,
+    });
+  }
+  // function handleSubmit() {
+  //   const newStaff = {
+  //     name: name,
+  //     doB: doB,
+  //     startDate: startDate,
+  //     department: department,
+  //     salaryScale: salaryScale,
+  //     annualLeave: annualLeave,
+  //     overTime: overTime,
+  //     image: image,
+  //   };
+    // onAdd
+  // }
+  function validate(
+    name,
+    doB,
+    startDate,
+    department,
+    salaryScale,
+    annualLeave,
+    overTime, touched
+  ) {
+    const errors = {
+      name: "",
+      doB: "",
+      startDate: "",
+      department: "",
+      salaryScale: "",
+      annualLeave: "",
+      overTime: "",
+      touched: {
+        name: false,
+        doB: false,
+        startDate: false, 
+        salaryScale: false,
+        department: false,
+        annualLeave: false,
+        overTime: false,
+      }
+     };
+    if (errors.touched.name && name.length < 3) {
+      errors.name = "name should be >=3 chars";
+    } else if (errors.touched.name && name.length > 50) {
+      errors.name = " name should be <= 50 chars";
+    }
+    if (errors.touched.department && department.length < 1) {
+      errors.department = "Vui lòng nhập thông tin";
+    }
+    if (errors.touched.salaryScale && salaryScale.length < 1) {
+      errors.salaryScale = "Vui lòng nhập thông tin";
+    }
+    if (errors.touched.annualLeave && annualLeave.length < 1) {
+      errors.annualLeave = "Vui lòng nhập thông tin";
+    }
+    if (errors.touched.overTime && overTime.length < 1) {
+      errors.overTime = "Vui lòng nhập thông tin";
+    }
+    if (errors.touched.startDate && startDate.length < 1) {
+      errors.startDate = "Vui lòng nhập thông tin";
+    }
+    if (errors.touched.doB && doB.length < 1) {
+      errors.doB = "Vui lòng nhập thông tin";
+    }
+    debugger
+    return errors;
   }
 
   function RenderStaffList({ Staff }) {
@@ -131,7 +185,6 @@ function StaffList(props, iStaffs, submitForm) {
           key={departmentItem.id}
         >
           <div
-            
             className="col-sm-12 col-md-3 col-xl-2"
             style={{
               padding: 1 + "em",
@@ -173,6 +226,8 @@ function StaffList(props, iStaffs, submitForm) {
       });
     }
   });
+  const errors=validate()
+  debugger
 
   return (
     <div className="container-fluid">
@@ -209,143 +264,170 @@ function StaffList(props, iStaffs, submitForm) {
           <Button color="primary" onClick={toggle1}>
             ADD
           </Button>
-        
 
-        <Modal
-          style={{ width: "900px", maxWidth: "100%" }}
-          isOpen={modalAdd}
-          toggle={toggle1}
-        >
-           <div>
-      <Form className="form-container" id="form-container" >
-        <ModalHeader>Thông tin nhân viên</ModalHeader>
-        <ModalBody >
-            <FormGroup className="row" id="form-group">
-                <Label className="col-sm-12 col-md-4 col-xl-3" for="name">Họ Và Tên</Label>
-                <Input
-                className="col-sm-12 col-md-8 col-xl-9"
-                  type="text"
-                  id="name"
-                  name="name"
-                  placeholder="Họ và tên"
-                  value={name}
-                  valid={errors.name===""}
-                  invalid ={errors.name !==""}
-                  onBlur={handleBlur("name")}
-                  onChange={handleInputChange}
-                />
-                <FormFeedback>{errors.name}</FormFeedback>
-            </FormGroup>
-            <FormGroup className="row" id="form-group">
-                <Label className="col-sm-12 col-md-4 col-xl-3" for="doB">Ngày Sinh</Label>
-                <Input
-                  className="col-sm-12 col-md-8 col-xl-9"
-                  type="date"
-                  id="doB"
-                  name="doB"
-                  value={doB}
-                  valid={errors.doB===""}
-                  invalid ={errors.doB !==""}
-                  onBlur={handleBlur("doB")}
-                  onChange={handleInputChange}
-                />
-               <FormFeedback>{errors.doB}</FormFeedback>
-            </FormGroup>
-            <FormGroup className="row" id="form-group">
-                <Label className="col-sm-12 col-md-4 col-xl-3" for="startDate">Ngày vào công ty</Label>
-                <Input
-                  className="col-sm-12 col-md-8 col-xl-9"
-                  type="date"
-                  id="startDate"
-                  name="startDate"
-                  value={startDate}
-                  valid={errors.startDate===""}
-                  invalid ={errors.startDate !==""}
-                  onBlur={handleBlur("startDate")}
-                  onChange={handleInputChange}
-                />
-                <FormFeedback>{errors.startDate}</FormFeedback>
-            </FormGroup>
-            <FormGroup className="row" id="form-group">
-                <Label className="col-sm-12 col-md-4 col-xl-3" for="department">Phòng ban</Label>
-                <Input className="col-sm-12 col-md-8 col-xl-9" type="select" name="department">
-                  {props.Departments.map((department) => {
-                    return (
-                      <option
-                        key={props.Departments.indexOf(department)}
-                        value={props.Departments.indexOf(department)}
-                      >
-                        {department.name}
-                      </option>
-                    );
-                  })}
-                </Input>
-            </FormGroup>
-            <FormGroup className="row" id="form-group">
-                <Label className="col-sm-12 col-md-4 col-xl-3" for="salaryScale">Hệ số lương</Label>
-                <Input
-                  className="col-sm-12 col-md-8 col-xl-9"
-                  type="number"
-                  defaultValue={1}
-                  name="salaryScale"
-                />
-            </FormGroup>
-            <FormGroup className="row" id="form-group">
-                <Label className="col-sm-12 col-md-4 col-xl-3">Số ngày nghỉ còn lại</Label>
-                <Input
-                  className="col-sm-12 col-md-8 col-xl-9"
-                  type="number"
-                  defaultValue={0}
-                  name="annualLeave"
-                />
-            </FormGroup>
-            <FormGroup className="row" id="form-group">
-                <Label className="col-sm-12 col-md-4 col-xl-3">Số ngày làm thêm</Label>
-                <Input
-                  className="col-sm-12 col-md-8 col-xl-9"
-                  type="number"
-                  defaultValue={0}
-                  name="overTime"
-                />
-            </FormGroup>
+          <Modal
+            style={{ width: "900px", maxWidth: "100%" }}
+            isOpen={modalAdd}
+            toggle={toggle1}
+          >
+            <div>
+              <Form className="form-container" id="form-container">
+                <ModalHeader>Thông tin nhân viên</ModalHeader>
+                <ModalBody>
+                  <FormGroup className="row" id="form-group">
+                    <Label className="col-sm-12 col-md-4 col-xl-3" for="name">
+                      Họ Và Tên
+                    </Label>
+                    <Input
+                      className="col-sm-12 col-md-8 col-xl-9"
+                      type="text"
+                      id="name"
+                      name="name"
+                      placeholder="Họ và tên"
+                      // defaultValue={name}
+                      valid={errors.name === ""}
+                      invalid={errors.name !== ""}
+                      onBlur={handleBlur()}
+                      // onChange={handleInputChange}
+                    />
+                    <FormFeedback>{errors.name}</FormFeedback>
+                  </FormGroup>
+                  <FormGroup className="row" id="form-group">
+                    <Label className="col-sm-12 col-md-4 col-xl-3" for="doB">
+                      Ngày Sinh
+                    </Label>
+                    <Input
+                      className="col-sm-12 col-md-8 col-xl-9"
+                      type="date"
+                      id="doB"
+                      name="doB"
+                      // value={doB}
+                      valid={errors.doB === ""}
+                      invalid={errors.doB !== ""}
+                      onBlur={handleBlur("doB")}
+                      // onChange={handleInputChange}
+                    />
+                    <FormFeedback>{errors.doB}</FormFeedback>
+                  </FormGroup>
+                  <FormGroup className="row" id="form-group">
+                    <Label
+                      className="col-sm-12 col-md-4 col-xl-3"
+                      for="startDate"
+                    >
+                      Ngày vào công ty
+                    </Label>
+                    <Input
+                      className="col-sm-12 col-md-8 col-xl-9"
+                      type="date"
+                      id="startDate"
+                      name="startDate"
+                      // value={startDate}
+                      valid={errors.startDate === ""}
+                      invalid={errors.startDate !== ""}
+                      onBlur={handleBlur("startDate")}
+                      // onChange={handleInputChange}
+                    />
+                    <FormFeedback>{errors.startDate}</FormFeedback>
+                  </FormGroup>
+                  <FormGroup className="row" id="form-group">
+                    <Label
+                      className="col-sm-12 col-md-4 col-xl-3"
+                      for="department"
+                    >
+                      Phòng ban
+                    </Label>
+                    <Input
+                      className="col-sm-12 col-md-8 col-xl-9"
+                      type="select"
+                      name="department"
+                    >
+                      {props.Departments.map((department) => {
+                        return (
+                          <option
+                            key={props.Departments.indexOf(department)}
+                            value={props.Departments.indexOf(department)}
+                          >
+                            {department.name}
+                          </option>
+                        );
+                      })}
+                    </Input>
+                  </FormGroup>
+                  <FormGroup className="row" id="form-group">
+                    <Label
+                      className="col-sm-12 col-md-4 col-xl-3"
+                      for="salaryScale"
+                    >
+                      Hệ số lương
+                    </Label>
+                    <Input
+                      className="col-sm-12 col-md-8 col-xl-9"
+                      type="number"
+                      defaultValue={1}
+                      name="salaryScale"
+                    />
+                  </FormGroup>
+                  <FormGroup className="row" id="form-group">
+                    <Label className="col-sm-12 col-md-4 col-xl-3">
+                      Số ngày nghỉ còn lại
+                    </Label>
+                    <Input
+                      className="col-sm-12 col-md-8 col-xl-9"
+                      type="number"
+                      defaultValue={0}
+                      name="annualLeave"
+                    />
+                  </FormGroup>
+                  <FormGroup className="row" id="form-group">
+                    <Label className="col-sm-12 col-md-4 col-xl-3">
+                      Số ngày làm thêm
+                    </Label>
+                    <Input
+                      className="col-sm-12 col-md-8 col-xl-9"
+                      type="number"
+                      defaultValue={0}
+                      name="overTime"
+                    />
+                  </FormGroup>
+                </ModalBody>
+                <ModalFooter>
+                  <Button type="submit" color="primary" onClick={toggle1}>
+                    Thêm
+                  </Button>
+                </ModalFooter>
+              </Form>
+            </div>
+          </Modal>
+        </div>
 
-        </ModalBody>
-        <ModalFooter>
-          <Button type="submit" color="primary" onClick={toggle1}>
-            Thêm
+        {/*Sort*/}
+        <div className="col-sm-7 col-md-8 col-xl-8">
+          <select className="form-select rounded " id="Defragment-select">
+            <option value="Default">None Defragment</option>
+            <option value="Defragment">Defragment with Deparment</option>
+          </select>
+        </div>
+        <div className="col-sm-3 col-md-2 col-xl-2">
+          <Button
+            type="button"
+            className="btn btn-outline-primary"
+            style={{
+              width: "100%",
+              background: "none",
+            }}
+            onClick={myDefragment}
+          >
+            {" "}
+            Submit
           </Button>
-        </ModalFooter>
-      </Form>
-    </div>
-        </Modal>
-      </div>
+        </div>
 
-      {/*Sort*/}
-      <div className="col-sm-7 col-md-8 col-xl-8">
-        <select className="form-select rounded " id="Defragment-select">
-          <option value="Default">None Defragment</option>
-          <option value="Defragment">Defragment with Deparment</option>
-        </select>
+        {/*body*/}
+        <div className="row">{DepartmentContainer}</div>
+        <div className="row">
+          Bấm vào tên Nhân Viên để xem thông tin cụ thể.
+        </div>
       </div>
-      <div className="col-sm-3 col-md-2 col-xl-2">
-        <Button
-          type="button"
-          className="btn btn-outline-primary"
-          style={{
-            width: "100%",
-            background: "none",
-          }}
-          onClick={myDefragment}
-        >
-          {" "}
-          Submit
-        </Button>
-      </div>
-
-      {/*body*/}
-      <div className="row">{DepartmentContainer}</div>
-      <div className="row">Bấm vào tên Nhân Viên để xem thông tin cụ thể.</div>
-    </div>
     </div>
   );
 }
